0d21a09d09c27f9b26de8906184138fbf439b1dd
monpase007 <urman7994@gmail.com>
2021-01-25 00:26:22 +0300

    Updates

---
+++ precache-manifest.74241547550923cc89d787d96912aa0e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9542bde32dbf057c3c15d0676b909056",
+    "url": "/Praktika/index.html"
+  },
+  {
+    "revision": "0f38c921e51dd369f1f6",
+    "url": "/Praktika/static/css/main.cbcabe33.chunk.css"
+  },
+  {
+    "revision": "f5d84297791dce960080",
+    "url": "/Praktika/static/js/2.5ec50fee.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/Praktika/static/js/2.5ec50fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f38c921e51dd369f1f6",
+    "url": "/Praktika/static/js/main.fcacc40f.chunk.js"
+  },
+  {
+    "revision": "d034658e98599852fa01",
+    "url": "/Praktika/static/js/runtime-main.e1345228.js"
+  },
+  {
+    "revision": "5487bea91cef82008fdc6e874b4c5c86",
+    "url": "/Praktika/static/media/OpenSans-Bold.5487bea9.eot"
+  },
+  {
+    "revision": "759ea19bccf8a5ca50a58e474aced157",
+    "url": "/Praktika/static/media/OpenSans-Bold.759ea19b.woff"
+  },
+  {
+    "revision": "8eb005f737c1e60d2e4df01c70913aaf",
+    "url": "/Praktika/static/media/OpenSans-Bold.8eb005f7.svg"
+  },
+  {
+    "revision": "93fe8f3415688d16bb947e2c96ea380e",
+    "url": "/Praktika/static/media/OpenSans-Bold.93fe8f34.woff2"
+  },
+  {
+    "revision": "ab38fda09644c992dc09754c3cc78201",
+    "url": "/Praktika/static/media/OpenSans-Bold.ab38fda0.ttf"
+  },
+  {
+    "revision": "38ca9e2302c8793c93f854e8595eed78",
+    "url": "/Praktika/static/media/OpenSans-Regular.38ca9e23.eot"
+  },
+  {
+    "revision": "5d5735e57127db2f7a2ad879fc6056b8",
+    "url": "/Praktika/static/media/OpenSans-Regular.5d5735e5.woff2"
+  },
+  {
+    "revision": "6fde2eb6728eb97fd914dcd0641d332b",
+    "url": "/Praktika/static/media/OpenSans-Regular.6fde2eb6.woff"
+  },
+  {
+    "revision": "8df00d6176ddae387d9431d3307b3e62",
+    "url": "/Praktika/static/media/OpenSans-Regular.8df00d61.ttf"
+  },
+  {
+    "revision": "bbbd47d2660d3547e98a864324874080",
+    "url": "/Praktika/static/media/OpenSans-Regular.bbbd47d2.svg"
+  },
+  {
+    "revision": "0423f1b79486132e4e5f39c305ab11f1",
+    "url": "/Praktika/static/media/icomoon.0423f1b7.ttf"
+  },
+  {
+    "revision": "65b5f5ad4a0951c339e72aa3976ebd85",
+    "url": "/Praktika/static/media/icomoon.65b5f5ad.svg"
+  },
+  {
+    "revision": "80bec9a18856394471577659385eb0ee",
+    "url": "/Praktika/static/media/icomoon.80bec9a1.eot"
+  },
+  {
+    "revision": "eb9982cebd25c39147b53851c96909f3",
+    "url": "/Praktika/static/media/icomoon.eb9982ce.woff"
+  },
+  {
+    "revision": "6c9609f2f9fc1e736615cc8a008630bb",
+    "url": "/Praktika/static/media/icon-avatar-16.6c9609f2.jpg"
+  },
+  {
+    "revision": "8ef2f432726887c3b0bd6008f20c7a42",
+    "url": "/Praktika/static/media/noteBg.8ef2f432.jpg"
+  },
+  {
+    "revision": "0e4099c30b9127d5fe2146b50e07025d",
+    "url": "/Praktika/static/media/preloader.0e4099c3.svg"
+  },
+  {
+    "revision": "9f8ef9760085fd6a7b1e445b9d9b0874",
+    "url": "/Praktika/static/media/warning.9f8ef976.png"
+  }
+]);
